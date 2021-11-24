42e462898d87a33839d84fe3bd867333032c54a6
Byung Wook Park <bbwwpark@naver.com>
2021-11-24 17:52:43 +0900

    feat: TypeQuery, Query 사용해보기

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -14,10 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
 
-           Member member = new Member();
-           member.setUsername("member1");
-           em.persist(member);
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username, m.age from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
 
             tx.commit();
         } catch (Exception e) {
