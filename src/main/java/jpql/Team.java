4fd9740a08096776794bc83ece1045b54ce99aba
Byung Wook Park <bbwwpark@naver.com>
2021-11-24 17:48:51 +0900

    feat: 실습용 DB 스키마 구현

---
+++ src/main/java/jpql/Team.java
@@ -0,0 +1,47 @@
+package jpql;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> mebers = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
