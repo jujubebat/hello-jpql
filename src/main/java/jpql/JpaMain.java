20e07cc91d279529cb96d70cd6d17fca69e6aca6
Byung Wook Park <bbwwpark@naver.com>
2021-11-25 17:02:16 +0900

    feat: 페이징 API를 학습한다.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -15,30 +15,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            // 1. Qeury 타입으로 조회
-//            List resultList = em.createQuery("select distinct m.username, m.age from Member m").getResultList();
-//            Object o = resultList.get(0);
-//            Object[] result = (Object[]) o;
-
-            // 2. Object[] 타입으로 조회
-//            List<Object[]> resultList = em.createQuery("select distinct m.username, m.age from Member m").getResultList();
-//            Object[] result = resultList.get(0);
-
-            // 3. new 명령어로 조회
-            List<MemberDTO> resultList = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                .setFirstResult(1)
+                .setMaxResults(10)
                 .getResultList();
-            MemberDTO memberDTO = resultList.get(0);
 
-            System.out.println("username = " + memberDTO.getUsername());
-            System.out.println("age = " + memberDTO.getAge());
+            System.out.println("result.size = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
