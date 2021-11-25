4b01b8cf50910b73e366bb8b23cafd09d4d647d9
Byung Wook Park <bbwwpark@naver.com>
2021-11-25 16:35:12 +0900

    feat: 엔티티 프로젝션을 학습한다.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,10 +1,10 @@
 package jpql;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -19,12 +19,18 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query.setParameter("username", "member1");
-            Member singleResult = query.getSingleResult();
+            em.flush();
+            em.clear();
 
-            System.out.println("singleResult = " + singleResult);
-            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+            // 아래와 같이 엔티티를 조회할 수 있다.(엔티티 조회) 엔티티 조회를 한 엔티티는 영속성 컨텍스트에서 관리된다!
+            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
+
+            // Team 엔티티를 가져오기 위해 join 쿼리가 나간다.
+            // 참고로, 아래와 같이 작성하지 말고, join을 직접 사용해서 join이 된다는 것을 명시적으로 표햔하는게 좋다.
+            // List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
