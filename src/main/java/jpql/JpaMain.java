976ee93dcce02b5530a5d19a4ec935e0c26bbf09
Byung Wook Park <bbwwpark@naver.com>
2021-11-25 16:36:32 +0900

    feat: 임베디드 타입 프로젝션을 학습한다.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,5 @@
 package jpql;
 
-import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -22,15 +21,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            // 아래와 같이 엔티티를 조회할 수 있다.(엔티티 조회) 엔티티 조회를 한 엔티티는 영속성 컨텍스트에서 관리된다!
-            List<Member> result = em.createQuery("select m from Member m", Member.class).getResultList();
-
-            // Team 엔티티를 가져오기 위해 join 쿼리가 나간다.
-            // 참고로, 아래와 같이 작성하지 말고, join을 직접 사용해서 join이 된다는 것을 명시적으로 표햔하는게 좋다.
-            // List<Team> result = em.createQuery("select m.team from Member m", Team.class).getResultList();
-
-            Member findMember = result.get(0);
-            findMember.setAge(20);
+            em.createQuery("select o.address from Order o", Address.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
